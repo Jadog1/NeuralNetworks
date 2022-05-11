31f9447dc098c9a34a78fb9474810fde46bd59d8
Jadog1 <steijr03@pfw.edu>
2022-05-10 22:15:23 -0400

    Fixed cmake files

---
+++ NeuralNetworks.cpp
@@ -0,0 +1,12 @@
+﻿// NeuralNetworks.cpp : Defines the entry point for the application.
+//
+
+#include "headers\NeuralNetworks.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
